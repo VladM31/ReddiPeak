940c6420aeb17ae1c0660b2d3dac00d5d693892d
vladyslav.mormu <vladyslav.mormul@nure.ua>
2023-06-02 21:54:59 +0300

    added decorator for HttpUrl #2

--- app/src/main/java/app/android/peak/reddit/utils/Utils.kt
+++ app/src/main/java/app/android/peak/reddit/utils/Utils.kt
@@ -1,9 +1,17 @@
 package app.android.peak.reddit.utils
 
+import okhttp3.HttpUrl
 import java.util.*
 import java.util.concurrent.TimeUnit
 
 fun hoursSince(unixTime: Long): Long {
     val now = Date().time
     return TimeUnit.MILLISECONDS.toHours(now - unixTime)
+}
+
+fun HttpUrl.newBuilder(pagination: RedditPaginationPosts): HttpUrl.Builder {
+    val newUtlBuilder = this.newBuilder()
+
+    return pagination.after.let { newUtlBuilder.addQueryParameter("after", it) }
+        .addQueryParameter("limit", pagination.limit.toString())
 }
