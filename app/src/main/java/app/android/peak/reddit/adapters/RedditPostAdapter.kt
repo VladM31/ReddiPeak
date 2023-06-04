e5d224b81e40c273ecb13cdc176637e194910250
vladyslav.mormu <vladyslav.mormul@nure.ua>
2023-06-04 14:26:40 +0300

    added RedditPostAdapter #5

---
+++ app/src/main/java/app/android/peak/reddit/adapters/RedditPostAdapter.kt
@@ -0,0 +1,75 @@
+package app.android.peak.reddit.adapters
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.paging.PagingDataAdapter
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.RecyclerView
+import app.android.peak.reddit.R
+import app.android.peak.reddit.models.Post
+import app.android.peak.reddit.utils.GlideHiddenRequestListener
+import app.android.peak.reddit.utils.isUrl
+import com.bumptech.glide.Glide
+
+class RedditPostAdapter(
+    private val onClick: (String) -> Unit
+) :
+    PagingDataAdapter<Post, RedditPostAdapter.ViewHolder>(POST_COMPARATOR) {
+
+
+    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        private val imageView: ImageView = itemView.findViewById(R.id.thumbnailImageView)
+        private val authorView: TextView = itemView.findViewById(R.id.authorItemTextView)
+        private val numCommentsView: TextView = itemView.findViewById(R.id.numCommentsTextView)
+        private val timeAgoView: TextView = itemView.findViewById(R.id.timeAgoTextView)
+
+        fun bind(post: Post) {
+            authorView.text = post.author
+            "Comments: ${post.numComments}".also { numCommentsView.text = it }
+            "${post.hoursAgo} hours ago".also { timeAgoView.text = it }
+
+            if (!isUrl(post.thumbnail)) {
+                imageView.visibility = View.GONE
+                return
+            }
+            imageView.visibility = View.VISIBLE
+
+
+            Glide.with(itemView)
+                .load(post.thumbnail)
+                .listener(GlideHiddenRequestListener(imageView))
+                .placeholder(R.drawable.photo_drawable)
+                .into(imageView)
+
+            imageView.setOnClickListener {
+                if(!post.isVideo){
+                    onClick(post.url!!)
+                }
+            }
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.post_item_activity, parent, false)
+        return ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val post = getItem(position)
+        post?.let { holder.bind(it) }
+    }
+
+
+    companion object {
+        private val POST_COMPARATOR = object : DiffUtil.ItemCallback<Post>() {
+            override fun areItemsTheSame(oldItem: Post, newItem: Post): Boolean =
+                oldItem.id == newItem.id
+
+            override fun areContentsTheSame(oldItem: Post, newItem: Post): Boolean =
+                oldItem == newItem
+        }
+    }
+}
