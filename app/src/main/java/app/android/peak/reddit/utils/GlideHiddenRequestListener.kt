ddc33473846df1f064f04a2d1c3a589a0d619bb2
vladyslav.mormu <vladyslav.mormul@nure.ua>
2023-06-04 14:26:56 +0300

    added GlideHiddenRequestListener #5

---
+++ app/src/main/java/app/android/peak/reddit/utils/GlideHiddenRequestListener.kt
@@ -0,0 +1,34 @@
+package app.android.peak.reddit.utils
+
+import android.graphics.drawable.Drawable
+import android.view.View
+import android.widget.ImageView
+import com.bumptech.glide.load.DataSource
+import com.bumptech.glide.load.HttpException
+import com.bumptech.glide.load.engine.GlideException
+import com.bumptech.glide.request.RequestListener
+import com.bumptech.glide.request.target.Target
+
+class GlideHiddenRequestListener(private val imageView : ImageView) : RequestListener<Drawable> {
+    override fun onLoadFailed(
+        e: GlideException?,
+        model: Any?,
+        target: Target<Drawable>?,
+        isFirstResource: Boolean
+    ): Boolean {
+        imageView.visibility = View.GONE
+        return false
+    }
+
+    override fun onResourceReady(
+        resource: Drawable?,
+        model: Any?,
+        target: Target<Drawable>?,
+        dataSource: DataSource?,
+        isFirstResource: Boolean
+    ): Boolean {
+        return false
+    }
+
+
+}
