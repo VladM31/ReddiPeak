02948bdf399942069d8bdda95a1f8317a6ae044f
vladyslav.mormu <vladyslav.mormul@nure.ua>
2023-06-04 23:23:40 +0300

    added ImageViewerActivity, with logic check permission #6

---
+++ app/src/main/java/app/android/peak/reddit/RedditPeakApp.kt
@@ -0,0 +1,9 @@
+package app.android.peak.reddit
+
+import android.app.Application
+
+class RedditPeakApp : Application() {
+    companion object {
+        const val IMAGE_URL = "IMAGE_URL"
+    }
+}
