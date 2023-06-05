d3e5ffa174a018b1b531cd7738d117fe93c93bbd
vladyslav.mormu <vladyslav.mormul@nure.ua>
2023-06-05 15:09:18 +0300

    updated MainActivity #close #9

--- app/src/main/java/app/android/peak/reddit/MainActivity.kt
+++ app/src/main/java/app/android/peak/reddit/MainActivity.kt
@@ -3,21 +3,16 @@ package app.android.peak.reddit
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.lifecycleScope
-import androidx.paging.Pager
-import androidx.paging.PagingConfig
 import androidx.recyclerview.widget.RecyclerView
 import app.android.peak.reddit.activities.ImageViewerActivity
 import app.android.peak.reddit.adapters.RedditPostAdapter
-import app.android.peak.reddit.clients.HttpRedditPostClient
-import app.android.peak.reddit.clients.RedditPostClient
-import app.android.peak.reddit.pagingsource.RedditPagingSource
-import com.google.gson.Gson
+import app.android.peak.reddit.viewmodels.RedditViewModel
 import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.launch
-import okhttp3.HttpUrl
-import okhttp3.OkHttpClient
-import java.util.concurrent.TimeUnit
+
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -25,7 +20,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-
+        val viewModel: RedditViewModel = ViewModelProvider(this)[RedditViewModel::class.java]
 
         val adapter = RedditPostAdapter(
             getString(R.string.forbiddenUrlPrefix),
@@ -40,38 +35,11 @@ class MainActivity : AppCompatActivity() {
 
         findViewById<RecyclerView>(R.id.recyclerView).adapter = adapter
 
-        val pager = Pager(
-            config = PagingConfig(
-                pageSize = 20,
-                enablePlaceholders = false
-            ),
-            pagingSourceFactory = { RedditPagingSource(makeClient()) }
-        ).flow
-
         lifecycleScope.launch {
-            pager.collectLatest { pagingData ->
+            viewModel.getPosts().collectLatest { pagingData ->
                 adapter.submitData(pagingData)
             }
         }
     }
 
-    private fun makeClient(): RedditPostClient {
-        return HttpRedditPostClient(
-            Gson(),
-            OkHttpClient.Builder()
-                .connectTimeout(60, TimeUnit.SECONDS)
-                .readTimeout(60, TimeUnit.SECONDS)
-                .writeTimeout(60, TimeUnit.SECONDS)
-                .build(),
-            getRedditPostUrl()
-        )
-    }
-
-    private fun getRedditPostUrl(): HttpUrl {
-        return HttpUrl.Builder()
-            .scheme(getString(R.string.scheme))
-            .host(getString(R.string.host))
-            .addPathSegment(getString(R.string.pathSegment))
-            .build()
-    }
 }
